8de0dbc29d824be2777c1e6db208846cd9840768
HadenLee <188708@gmail.com>
2022-07-01 23:51:33 -0400

    main

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,10 +5,7 @@ export const Footer = () => {
       <div id='footer'>
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
-            </a>
+            &copy; HADEN LEE 2022
           </p>
         </div>
       </div>
